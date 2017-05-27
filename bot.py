6850b3c900a646d9dcbb47a6c344d3edeaedbb0f
Angelina Nikiforova <nikiforova.angelina@gmail.com>
2017-05-27 23:57:29 +0300

    bot modified to terms of ISO13485

--- bot.py
+++ bot.py
@@ -1,17 +1,34 @@
 import keys
+from iso import iso
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 def greet_user(bot, update):
-    text = 'Called /start'
-    update.message.reply_text(text)
+    print('Called /start')
+    lst_terms = []
+    for d_key in iso:
+        lst_terms.append(d_key)
+    all_terms = ', \n'.join(lst_terms)
+    update.message.reply_text(all_terms)
 
 
 def talk_to_me(bot, update):
-    user_text = update.message.text
+    user_text = update.message.text.lower()
     print(user_text)
-    update.message.reply_text(user_text)
+    possible_keys = []
+    for d_key in iso:
+        if user_text in d_key:
+            possible_keys.append(d_key)
+
+    if len(possible_keys) == 0:
+        reply = 'Такого определения нет, может опечатка?'
+    elif len(possible_keys) == 1:
+        reply = possible_keys[0] + ' – это ' + iso.get(possible_keys[0])
+    else:
+        possible_replies = ', '.join(possible_keys)
+        reply = 'Есть несколько определений, уточни запрос: ' + possible_replies
+    update.message.reply_text(reply)
 
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
